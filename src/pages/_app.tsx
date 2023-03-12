04616a288f56552cbf37a4e258e5f2ec8de5580b
Yunwoo Ji <unu12073@gmail.com>
2023-03-12 20:18:26 +0900

    구글 애드센스 스크립트 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -164,8 +164,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
+        async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440044743501222"
         crossOrigin="anonymous"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
       />
       <ThemeProvider theme={theme}>
         <Provider store={store}>
