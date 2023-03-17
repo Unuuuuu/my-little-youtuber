6965b88fa03110b88bf9ea4e9ad072c9af9af302
Yunwoo Ji <unu12073@gmail.com>
2023-03-17 18:18:13 +0900

    pretendard 셋업

--- src/app/components/Provider.tsx
+++ src/app/components/Provider.tsx
@@ -3,6 +3,18 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { store } from "@/lib/store";
 import { Provider as ReduxProvider } from "react-redux";
+import localFont from "next/font/local";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const pretendard = localFont({
+  src: "./PretendardVariable.woff2",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: pretendard.style.fontFamily,
+  },
+});
 
 interface Props {
   children: React.ReactNode;
@@ -14,7 +26,9 @@ export default function Provider(props: Props) {
   return (
     <>
       <CssBaseline />
-      <ReduxProvider store={store}>{children}</ReduxProvider>
+      <ThemeProvider theme={theme}>
+        <ReduxProvider store={store}>{children}</ReduxProvider>
+      </ThemeProvider>
     </>
   );
 }
