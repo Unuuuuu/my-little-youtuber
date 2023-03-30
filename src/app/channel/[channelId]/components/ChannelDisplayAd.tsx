3fc87333fa124cc51760e7a52f0c831711e134a7
Yunwoo Ji <unu12073@gmail.com>
2023-03-30 12:57:50 +0900

    홈과 채널 광고 수정

--- src/app/channel/[channelId]/components/ChannelDisplayAd.tsx
+++ src/app/channel/[channelId]/components/ChannelDisplayAd.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Script from "next/script";
 import { useEffect } from "react";
 
-export default function HomeDisplayAd() {
+function Left() {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -13,7 +15,7 @@ export default function HomeDisplayAd() {
   }, []);
 
   return (
-    <div>
+    <>
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440044743501222"
@@ -21,12 +23,69 @@ export default function HomeDisplayAd() {
       />
       <ins
         className="adsbygoogle"
-        style={{ display: "block", textAlign: "center", height: "50px" }}
+        style={{
+          display: "block",
+          textAlign: "center",
+          height: "50px",
+          flex: 1,
+        }}
         data-ad-client="ca-pub-4440044743501222"
         data-ad-slot="1129126488"
         data-ad-format="horizontal"
         data-full-width-responsive="false"
       />
-    </div>
+    </>
+  );
+}
+
+function Right() {
+  const isMatch = useMediaQuery("(max-width:468px)");
+
+  useEffect(() => {
+    if (isMatch) {
+      return;
+    }
+
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.error(error);
+    }
+  }, [isMatch]);
+
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440044743501222"
+        crossOrigin="anonymous"
+      />
+      <ins
+        className="adsbygoogle"
+        style={
+          isMatch
+            ? { display: "none" }
+            : {
+                display: "block",
+                textAlign: "center",
+                height: "50px",
+                flex: 1,
+              }
+        }
+        data-ad-client="ca-pub-4440044743501222"
+        data-ad-slot="9568328638"
+        data-ad-format="horizontal"
+        data-full-width-responsive="false"
+      />
+    </>
+  );
+}
+
+export default function ChannelDisplayAd() {
+  return (
+    <Box sx={{ position: "relative", zIndex: 1, display: "flex" }}>
+      <Left />
+      <Right />
+    </Box>
   );
 }
--- src/app/components/HomeDisplayAd.tsx
+++ src/app/components/HomeDisplayAd.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Script from "next/script";
 import { useEffect } from "react";
 
-export default function HomeDisplayAd() {
+function Left() {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -14,7 +15,7 @@ export default function HomeDisplayAd() {
   }, []);
 
   return (
-    <Box sx={{ position: "relative", zIndex: 1 }}>
+    <>
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440044743501222"
@@ -22,12 +23,69 @@ export default function HomeDisplayAd() {
       />
       <ins
         className="adsbygoogle"
-        style={{ display: "block", textAlign: "center", height: "50px" }}
+        style={{
+          display: "block",
+          textAlign: "center",
+          height: "50px",
+          flex: 1,
+        }}
         data-ad-client="ca-pub-4440044743501222"
         data-ad-slot="3360823567"
         data-ad-format="horizontal"
         data-full-width-responsive="false"
       />
+    </>
+  );
+}
+
+function Right() {
+  const isMatch = useMediaQuery("(max-width:468px)");
+
+  useEffect(() => {
+    if (isMatch) {
+      return;
+    }
+
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.error(error);
+    }
+  }, [isMatch]);
+
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4440044743501222"
+        crossOrigin="anonymous"
+      />
+      <ins
+        className="adsbygoogle"
+        style={
+          isMatch
+            ? { display: "none" }
+            : {
+                display: "block",
+                textAlign: "center",
+                height: "50px",
+                flex: 1,
+              }
+        }
+        data-ad-client="ca-pub-4440044743501222"
+        data-ad-slot="6012227007"
+        data-ad-format="horizontal"
+        data-full-width-responsive="false"
+      />
+    </>
+  );
+}
+
+export default function HomeDisplayAd() {
+  return (
+    <Box sx={{ position: "relative", zIndex: 1, display: "flex" }}>
+      <Left />
+      <Right />
     </Box>
   );
 }
