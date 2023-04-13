b346d490b3462b0db59574164dccf0cb64276ab2
Yunwoo Ji <unu12073@gmail.com>
2023-04-13 22:20:12 +0900

    홈 배너 업데이트

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -7,6 +7,7 @@ import bgImageSrc from "@/assets/hero-section-bg-image.svg";
 import Image from "next/image";
 import HomeDisplayAd from "./HomeDisplayAd";
 import youtubeLogoSrc from "@/assets/hero-youtube-logo.svg";
+import coffeeSrc from "@/assets/coffee.png";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { logEvent } from "firebase/analytics";
@@ -49,10 +50,10 @@ export default function HeroSection() {
             position: "relative",
             width: "100%",
             height: 270,
-            pt: "54px",
+            pt: "35px",
           },
           isSm && {
-            pt: "40px",
+            pt: "38px",
           },
         ]}
         onClick={handleSurveyBannerClick}
@@ -84,77 +85,173 @@ export default function HeroSection() {
             alignItems: "center",
           }}
         >
+          <Typography
+            sx={[
+              {
+                fontSize: 16,
+                color: "#313131",
+                lineHeight: "30px",
+              },
+              isSm && {
+                fontSize: 20,
+                mb: "5px",
+              },
+            ]}
+          >
+            조회수 높은 영상 맞추기 게임
+          </Typography>
           <Box
-            sx={{
-              display: "flex",
-              gap: "18px",
-              alignItems: "center",
-              mb: "24px",
-            }}
+            sx={[
+              {
+                display: "flex",
+                alignItems: "center",
+                mb: "14px",
+                gap: "8px",
+              },
+              isSm && {
+                gap: "10px",
+                mb: "13px",
+              },
+            ]}
           >
             <Image
               src={youtubeLogoSrc}
               alt="youtube logo"
               style={
                 isSm
-                  ? { width: "70px", height: "48px" }
-                  : { width: "50px", height: "34px" }
+                  ? {
+                      width: "48px",
+                      height: "32px",
+                    }
+                  : { width: "36px", height: "24.5px" }
               }
             />
             <Typography
               fontWeight={800}
-              fontSize={isSm ? 42 : 28}
-              lineHeight={"46px"}
               color={"#313131"}
+              sx={[
+                { fontSize: 24, lineHeight: "46px" },
+                isSm && {
+                  fontSize: 36,
+                },
+              ]}
             >
               나의 작은 유튜버
             </Typography>
           </Box>
-          <Typography
-            fontSize={isSm ? 28 : 18}
-            textAlign="center"
-            color={"#241149"}
-            sx={{ mb: isSm ? "24px" : "30px" }}
+          <Box
+            sx={[
+              { position: "relative", mb: "21px" },
+              isSm && {
+                mb: "13px",
+              },
+            ]}
           >
-            서비스 개선을 위한 설문조사 참여하고
-            <br />
-            <Box component={"b"} fontWeight={600}>
-              커피 기프티콘
-            </Box>{" "}
-            받아가세요!
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center", gap: "12px" }}>
+            <Typography
+              textAlign="center"
+              color={"#241149"}
+              sx={[
+                {
+                  fontSize: 18,
+                  lineHeight: "28px",
+                },
+                isSm && {
+                  fontSize: 20,
+                },
+              ]}
+            >
+              서비스 개선을 위한 설문조사 참여하고
+              <br />
+              <Typography
+                component={"b"}
+                fontWeight={600}
+                sx={[
+                  {
+                    px: "2.5px",
+                    lineHeight: "23px",
+                    bgcolor: "rgb(254, 255, 212)",
+                    fontSize: 18,
+                  },
+                  isSm && {
+                    px: "4px",
+                    fontSize: 20,
+                  },
+                ]}
+              >
+                커피 기프티콘
+              </Typography>{" "}
+              받아가세요!
+            </Typography>
+            <Image
+              src={coffeeSrc}
+              alt="coffee"
+              width={isSm ? 50 : 39}
+              height={isSm ? 35 : 27}
+              style={
+                isSm
+                  ? { position: "absolute", bottom: -8, left: -12 }
+                  : { position: "absolute", bottom: -2, left: 1 }
+              }
+            />
+          </Box>
+          <Box
+            sx={[
+              { display: "flex", alignItems: "center", gap: "8px" },
+              isSm && {
+                gap: "5px",
+              },
+            ]}
+          >
             <Box
               sx={[
                 {
                   bgcolor: "#595068",
                   borderRadius: "99px",
-                  width: "95px",
-                  height: "24px",
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
+                  p: "4px 8.5px",
                 },
                 isSm && {
-                  width: "115px",
-                  height: "30px",
+                  p: "4.5px 10px",
                 },
               ]}
             >
               <Typography
                 color="white"
-                fontWeight={500}
-                fontSize={isSm ? 18 : 15}
+                sx={[
+                  { fontSize: 15, lineHeight: "16px" },
+                  isSm && {
+                    fontSize: 18,
+                    fontWeight: 500,
+                    lineHeight: "21px",
+                  },
+                ]}
               >
+                <Typography
+                  component={"b"}
+                  sx={[
+                    { color: "#FBFFD0", lineHeight: "16px", fontSize: 15 },
+                    isSm && {
+                      fontSize: 18,
+                      lineHeight: "21px",
+                      fontWeight: 500,
+                    },
+                  ]}
+                >
+                  2차
+                </Typography>{" "}
                 이벤트 기간
               </Typography>
             </Box>
             <Typography
               color="#242424"
-              fontWeight={500}
-              fontSize={isSm ? 18 : 15}
+              sx={[
+                { fontSize: 15, lineHeight: "30px" },
+                isSm && { fontSize: 18, fontWeight: 500, lineHeight: "42px" },
+              ]}
             >
-              4월 6일 ~ 4월 13일
+              4월 14일 ~ 4월 21일
             </Typography>
           </Box>
         </Box>
